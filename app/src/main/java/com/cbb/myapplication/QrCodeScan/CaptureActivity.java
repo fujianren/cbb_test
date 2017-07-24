b81148466ce5dcfc84338b947c5a2a69deb0dfc7
fujianren <979263361@qq.com>
2017-07-24 15:24:58 +0800

    一次失败的扫码so库

---
+++ app/src/main/java/com/cbb/myapplication/QrCodeScan/CaptureActivity.java
@@ -0,0 +1,13 @@
+package com.cbb.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaptureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+    }
+}
