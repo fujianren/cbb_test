821d6922a8eaa43770f677b30911d45d068fb0a8
fujianren <979263361@qq.com>
2017-07-31 14:43:12 +0800

    翻转动画的侧拉抽屉

---
+++ app/src/main/java/com/cbb/myapplication/SideMenuActivity.java
@@ -0,0 +1,13 @@
+package com.cbb.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SideMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side_menu);
+    }
+}
---
+++ app/src/main/java/com/cbb/myapplication/share/ShareActivity.java
@@ -0,0 +1,15 @@
+package com.cbb.myapplication.share;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cbb.myapplication.R;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
